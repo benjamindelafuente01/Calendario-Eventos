4774e077f51c325b053a74d001a4dc89c7645149
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-10-02 08:25:29 -0600

    Script con los datos para realizar la conexion a base de datos

---
+++ Connection/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*
+        Definimos las variables para la conexion mediante PDO
+    */
+
+    define ('DATA_SOURCE_NAME', 'mysql:host=localhost; dbname=');
+    define ('USER', 'root');
+    define ('PASSWORD', '');
+
+?>

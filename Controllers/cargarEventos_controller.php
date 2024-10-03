b3e3d2efa40b9ed1ffe965b67b769001f4ff00bc
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-10-03 15:08:58 -0600

    Controlador para consultar los eventos en base de datos, transformarlos a un JSON valido y enviarlos

---
+++ Controllers/cargarEventos_controller.php
@@ -0,0 +1,63 @@
+<?php
+
+    /*
+        Controlador donde se consultan los eventos de la base de datos y a través de la función
+        'convertirEventos()' se verifica que contenga eventos y se crea un arreglo que  en el 
+        formato que la vista puede recibir.
+    */
+
+    // Importamos el archivo de la clase que inserta un nuevo evento
+    require __DIR__ . '/../Classes/inicio_class.php';
+
+    // Especificamos que se va a devolver un JSON
+    header('Content-Type: application/json');
+
+    // Instanciamos la clase
+    $traerEventos = new Evento();
+
+    // Consultamos los eventos
+    $eventosBD = $traerEventos->traerEventos();
+
+    // Llamamos a la función para convertir arreglo de Base de datos a un arreglo que acepte FullCalendar
+    $eventosCalendario = convertirEventos($eventosBD);
+
+    // Convertir el nuevo arreglo a JSON (con echo devolvemos cada que se llame el script)
+    echo json_encode($eventosCalendario);
+
+
+    /*
+        Función para convertir el array de los eventos en un JSON valido
+        Recibe el arreglo de base de datos y lo convierte aun arreglo listo para convertir a JSON
+    */
+    function convertirEventos ($eventos) {
+
+        // Verificamos si es diferente de false
+        if ($eventos == false) {
+            // Regresamos un arreglo vacio
+            $eventosVacio = [];
+            return $eventosVacio;
+
+        } else {
+
+            // Array que contendrá los eventos
+            $eventosJson = array();
+            // Arrreglo auxiliar
+            $arregloAuxiliar = [];
+
+            // Recorremos arreglo para agregar eventos de forma individual con arreglo auxiliar
+            foreach($eventos as $evento) {
+                $arregloAuxiliar = array (
+                    'title' => $evento['title'],
+                    'start' => $evento['start'] .  'T' . $evento['hora_inicio'],
+                    'end' => $evento['end'] . 'T' . '23:59',
+                    'color' => $evento['color']
+                );
+                // Agregamos arreglo auxilixar al arreglo principal
+                array_push($eventosJson, $arregloAuxiliar);
+            }
+
+            return $eventosJson;
+        }
+    }
+
+?>

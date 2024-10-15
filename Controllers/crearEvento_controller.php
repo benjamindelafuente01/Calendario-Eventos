871b477cf0c875260345cfa0965ad224558c01f7
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-10-15 17:03:39 -0600

    Agregamos un input oculto para la hora de fin en el modal, en el controlador establecimos zona horaria, recuperamos valor de hora fin y agregamos los valores de fecha actual y usuario. En la clase agregamos los campos mencionados

--- Controllers/crearEvento_controller.php
+++ Controllers/crearEvento_controller.php
@@ -6,6 +6,9 @@
     // Especificamos que se va a devolver un JSON
     header('Content-Type: application/json');
 
+    // Establecemos la zona horario como CDMX
+    date_default_timezone_set('America/Mexico_City');
+
     // Verificamos la solicitud por POST
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
@@ -15,7 +18,13 @@
         $inicioEvento = filter_var($_POST['fecha_inicio_evento'], FILTER_SANITIZE_STRING);
         $finEvento = filter_var($_POST['fecha_fin_evento'], FILTER_SANITIZE_STRING);
         $horaInicioEvento = filter_var($_POST['hora_inicio_evento'], FILTER_SANITIZE_STRING);
+        $horaFinEvento = filter_var($_POST['hora_fin_evento'], FILTER_SANITIZE_STRING);
         $colorEvento = filter_var($_POST['color_evento'], FILTER_SANITIZE_STRING);
+
+        // Valor de hoy (fecha en que se creó)
+        $fecha_creacion = date("Y-m-d H:i:s");
+        // TODO: Valor temporal del usuario
+        $usuario = 'Benjamin';
 
         /*
             Enviamos datos del evento a la base de datos
@@ -25,7 +34,7 @@
         $guardarEvento = new Evento();
 
         // Insertamos datos
-        $guardarEvento->crearNuevoEvento ($nombreEvento, $inicioEvento, $finEvento, $colorEvento, $costoEvento, $horaInicioEvento);
+        $guardarEvento->crearNuevoEvento ($nombreEvento, $inicioEvento, $finEvento, $colorEvento, $costoEvento, $horaInicioEvento, $horaFinEvento, $usuario, $fecha_creacion);
 
         // Verificamos resultado de la consulta
         if ($guardarEvento) {

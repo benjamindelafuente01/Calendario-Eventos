4454d2547548e696744395114a3f2ba9be366abf
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-10-16 14:44:22 -0600

    Controlador para finalizar el evento

---
+++ Controllers/finalizarEvento_controller.php
@@ -0,0 +1,54 @@
+<?php
+
+    /*
+        Controlador que recibe el ID del evento que se desea finalizar
+    */
+
+    // Importamos clase
+    require __DIR__ . '/../Classes/eventos_class.php';
+
+    // Especificamos que la respuesta será un json
+    header('Contect-Type: application/json');
+
+    // Instanciamos un objeto de la clase
+    $finalizar = new Eventos();
+
+    // Verificamos que la petición haya sido mediante POST
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Obtenemos el contenido del cuerpo de la solicitud (un json)
+        $data = file_get_contents('php://input');
+
+        // Decodificamos el json en un arreglo asociativo
+        $datosDecodificados = json_decode($data, true);
+
+        // Guardamos ID
+        $id = $datosDecodificados['id'];
+
+        // Eliminamos evento
+        $eventoFinalizado = $finalizar->finalizarEvento($id);
+
+        // Verificamos resultado
+        if ($eventoFinalizado) {
+            $resultado = [
+                'exito' => true,
+                'mensaje' => 'Evento finalizado'
+            ];
+        } else {
+            $resultado = [
+                'exito' => false,
+                'mensaje' => 'Error al finalizar evento'
+            ];
+        }
+
+        // Enviamos json
+        echo json_encode($resultado);
+    
+    } else {
+        echo json_encode([
+            'exito' => false,
+            'mensaje' => 'Método no permitido'
+        ]);
+    }
+
+?>

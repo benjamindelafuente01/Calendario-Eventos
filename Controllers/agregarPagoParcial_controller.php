82b5be74f1eb6455fe06a4baf7d2d51e48cf7217
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-11-08 19:08:58 -0600

    Controlador para recibir un pago parcial, realizar los calculos para saber si se finiquito el boleto y enviar los datos para las respectivas consultas

---
+++ Controllers/agregarPagoParcial_controller.php
@@ -0,0 +1,65 @@
+<?php
+
+    /*
+        Controlador para agregar un pago parcial. Recibe los datos de un nuevo pago y guardamos, asi como
+        actualizamos datos del boleto
+    */
+    
+    // Importamos clase de conexion
+    require __DIR__ . '/../Classes/pagos_class.php';
+
+    // Establecemos la zona horaria como CDMX
+    date_default_timezone_set('America/Mexico_City');
+
+    // Especificamos que devolveremos un json
+    header('Content-type: application/json');
+
+    // Verificamos que la solicitud haya sido mediante POST
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Instanciamos objeto de la clase para agregar pago
+        $realizarPago = new PagoParcial();
+
+        // Accedemos a los valores del formulario y sanitizamos
+        $idBoleto = filter_var($_POST['id_boleto'], FILTER_SANITIZE_STRING);
+        $montoAbonado = filter_var($_POST['monto_pago_parcial'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $saldoRestante = filter_var($_POST['saldo_restante_pago_parcial'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+        // Almacenamos fecha de registro del pago
+        $fechaPago = date('Y-m-d');
+
+        // TODO: Agregamos usuario
+        $usuario = 'Benjamin';
+
+        // Verificamos si se realizo el pago completo
+        $finiquitado = $montoAbonado == $saldoRestante ? true : false;
+
+        // Intentamos realizar pago y actualizar boleto
+        try {
+
+            // Iniciamos la transaccion
+            $realizarPago->iniciarTransaccion();
+
+            // Guardamos pago parcial
+            $realizarPago->guardarPagoParcial($montoAbonado, $fechaPago, $usuario, $idBoleto);
+
+            // Actualizamos boleto asociado
+            $realizarPago->actualizarBoleto($idBoleto, $montoAbonado, $finiquitado);
+
+            // Guardamos transaccion
+            $realizarPago->confirmarTransaccion();
+
+            // Enviamos respuesta de exito
+            echo json_encode(['exito' => true, 'mensaje' => 'Pago realizado correctamente']);
+
+        } catch(Exception $e) {
+
+            // Si algo salio mal revertimos cambios
+            $realizarPago->revertirTransaccion();
+
+            // Enviamos respuesta de error
+            echo json_encode(['exito' => false, 'mensaje' => 'Error al guardar el pago']);
+        }
+    }
+
+?>

36d71fa232b1992ddf707074667884bd30a6ee69
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-11-08 19:07:14 -0600

    Funcion para validar el monto abonado, limpiar formulario, enviar formulario de pago parcial y procesar respuesta

--- Action/validarModalPagoParcial.js
+++ Action/validarModalPagoParcial.js
@@ -84,3 +84,136 @@ function cerrarModalPagoParcial() {
     }
 }
 
+
+/*
+    Funcion para validar el monto abonado en el pago parcial
+*/
+function limpiarTablaPagoParcial() {
+
+    // Limpiamos inputs del buscador
+    document.getElementById('buscar_por_usuario').value = '';
+    document.getElementById('buscar_por_evento').value = '';
+
+    // Limpiamos cuerpo de la tabla
+    document.getElementById('resultadosPagosPendientes').innerHTML = '';
+
+    // Ocultamos encabezado de la tabla
+    document.getElementById('contenedorTablaPagosPendientes').style.display = 'none';
+}
+
+/*
+    Funcion para validar el monto abonado en el pago parcial
+*/
+function validarCamposPagoParcial() {
+    
+    // Bandera para verificar
+    let formularioValido = true;
+
+    // Obtenemos los valores de saldo restante, monto abonado y div de validacion
+    const saldoRestante = parseFloat (document.getElementById('saldo_restante_pago_parcial').value);
+    const montoAbonado = document.getElementById('monto_pago_parcial');
+    const validacionMontoAbonado = document.getElementById('validarMontoPago');
+
+    // Verificamos que el monto abonado sea un numero entero con 2 decimales
+    if  (!/^\d+(\.\d{1,2})?$/.test(montoAbonado.value.trim())) {
+
+        // Clase de error en el input
+        montoAbonado.classList.add('is-invalid');
+        // Agregamos mensaje de error en el div de validacion
+        validacionMontoAbonado.innerHTML = 'Por favor, introduce una cantidad válida';
+        validacionMontoAbonado.classList.add('invalid-feedback')
+
+        formularioValido = false;
+
+    // Verificamos que sea mayor que cero y menor qye saldo restante y no sea una cadena cero
+    } else if (parseFloat(montoAbonado.value.trim()) <= 0 || parseFloat(montoAbonado.value.trim()) > saldoRestante) {
+
+        // Clase de error en el input
+        montoAbonado.classList.add('is-invalid');
+        // Agregamos mensaje de error en el div de validacion
+        validacionMontoAbonado.innerHTML = 'Por favor, introduce una cantidad mayor que 0 y menor que saldo restante';
+        validacionMontoAbonado.classList.add('invalid-feedback')
+
+        formularioValido = false;
+    }
+
+    return formularioValido;
+}
+
+
+/*
+    Funcion que se llama cuando se quiere enviar el formulario de pago parcial
+*/
+function validarFormularioPagoParcial() {
+
+    // Verificamos resultado
+    let formulario = validarCamposPagoParcial();
+
+    if (formulario) {
+
+        // Obtenemos el formulario
+        const formularioPagoParcial = document.getElementById('formularioPagoParcial');
+       
+        // Creamos un objeto 'FormData' con los datos del formulario
+        const datosFormulario = new FormData(formularioPagoParcial);
+
+        // Enviamos peticion fetch para guardar pago
+        fetch ('../Controllers/agregarPagoParcial_controller.php', {
+            method: 'POST',
+            body: datosFormulario
+        })
+
+        .then (response => response.text())   // Leer la respuesta como texto
+        .then (data => {
+            // console.log('Respuesta del servidor (antes de JSON):', data);
+
+            try {
+                // Intentamos convertir respuesta a json
+                let respuestaJson = JSON.parse(data);
+
+                // Verificamos la respuesta del servidor
+                if (respuestaJson.exito) {
+                    
+                    // Mensaje de exito
+                    Swal.fire({
+                        title: "Pago añadido!",
+                        text: "El pago se ha agregado correctamente",
+                        icon: "success",
+                        timer: 2000
+                    });
+
+                    // Limpiamos buscador y tabla
+                    limpiarTablaPagoParcial();
+
+                    // Cerramos modal
+                    cerrarModalPagoParcial();
+
+                } else {
+                    // Mensaje de error
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Algo salió mal al guardar el pago: " + respuestaJson.mensaje,
+                    });
+                }
+
+            } catch(e) {
+                // console.error('Error al procesar JSON:', e);
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Error al procesar la respuesta del servidor." 
+                });
+            }
+        })
+        .catch(error => {
+            // console.error('Error al enviar los datos:', error);
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Error al enviar los datos." 
+            });
+        });
+
+    }
+}

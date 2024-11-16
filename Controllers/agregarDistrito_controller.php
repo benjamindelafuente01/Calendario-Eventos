daccc1a4df8fb6d5587ddc6e5d351a30b07046b1
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-11-15 19:29:18 -0600

    Controlador para agregar un nuevo distrito

---
+++ Controllers/agregarDistrito_controller.php
@@ -0,0 +1,42 @@
+<?php
+
+    /*
+        Controlador para guardar un nuevo distrito
+    */
+
+    // Importamos clase con el metodo para guardar gasto
+    require __DIR__ . '/../Classes/distritos_class.php';
+    
+    // Especificamos que devolveremos un json
+    header('Content-type: application/json');
+
+    // Verificamos que el método de solicitud haya sido POST
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // Obtenemos los datos y los sanitizamos
+        $nombre_distrito = filter_var($_POST['nombre_distrito'], FILTER_SANITIZE_STRING);
+
+        // Instanciamos objeto de la clase
+        $distrito = new Distrito();
+
+        // Guardamos gasto
+        $guardarDistrito = $distrito->agregarDistrito($nombre_distrito);
+
+        // Verificamos el reusltado
+        if ($guardarDistrito) {
+            $resultado = [
+                'exito' => true,
+                'mensaje' => 'Distrito agregado correctamente'
+            ];
+        } else {
+            $resultado = [
+                'exito' => false,
+                'mensaje' => 'El distrito no se pudo agregar'
+            ];
+        }
+
+        // Enviamos JSON
+        echo json_encode($resultado);
+    }
+    
+?>
